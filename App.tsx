5e54baec7fcebdaec0ad095c9cce1a63c087312d
Nathan Tredgett <nathantredgett@gmail.com>
2020-03-23 15:19:02 +0000

    added initial styling

--- App.tsx
+++ App.tsx
@@ -4,16 +4,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./components/Home";
 import Test from "./components/Test";
+import { Header, ThemeProvider } from "react-native-elements";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Test" component={Test} />
-      </Stack.Navigator>
+      <ThemeProvider>
+        <Header
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          centerComponent={{ text: "MY TITLE", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+        />
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Test" component={Test} />
+        </Stack.Navigator>
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Tile, Text, Button } from "react-native-elements";
 
 export default ({ navigation }) => (
-  <View>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <Button title="Go to Test" onPress={() => navigation.navigate("Test")} />
-  </View>
+  <>
+    <Tile
+      title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dolore exercitationem"
+      featured
+      imageSrc={{ uri: "http://placekitten.com/200/300" }}
+      caption="Some Caption Text"
+    />
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ut similique
+      mollitia consequatur laboriosam natus animi, vel aliquid sequi cum
+      architecto ratione, commodi expedita dignissimos sed, a consequuntur
+      eligendi. Soluta.
+    </Text>
+    <Button onPress={() => navigation.navigate("Test")} title="Hey!" />
+  </>
 );
